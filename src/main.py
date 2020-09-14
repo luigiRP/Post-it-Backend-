f5af696ae89e666cb857f33bceafa505d4e49123
luigiRP <luigirosales84@gmail.com>
2020-09-14 19:58:03 +0000

    Made some changes

--- src/main.py
+++ src/main.py
@@ -33,64 +33,61 @@ def sitemap():
 
 @app.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    if User.get_user(id) is None:
+    user = User.get_user(id)
+    if not user:
         raise APIException('User not found', status_code=404)
-    else:
-        return User.get_user(id)
+    return user
 
 @app.route('/login', methods=['GET'])
 def get_user_by_email():
     body=request.get_json()
 
-    if User.get_user_by_email(body["email"],body["password"]) is "email":
-        raise APIException("email doesn't exist", status_code=404)
-    elif User.get_user_by_email(body["email"],body["password"]) is "password":
-        raise APIException("password for that email doesn't exist", status_code=404)
-    else:
-        return User.get_user_by_email(body["email"],body["password"])
+    user = User.get_user_by_email(body["email"],body["password"])
+    if not user:
+        raise APIException("Login failed", status_code=401)
+    return user
 
 @app.route('/users/<int:id>/socials', methods=['GET'])
 def get_all_socials(id):
-    if Social.get_all_socials(id) is None:
+    socials = Social.get_all_socials(id)
+    if not socials:
         raise APIException('Social media accounts not found', status_code=404)
-    else:
-
-        return jsonify(Social.get_all_socials(id))
+    return jsonify(socials)
 
 @app.route('/users/<int:id_user>/socials/<int:id_social>', methods=['GET'])
 def get_social(id_user,id_social):
-    if Social.get_social(id_user,id_social) is None:
+    social = Social.get_social(id_user,id_social)
+    if not social:
         raise APIException('Social media account not found', status_code=404) 
-    else:
-        return jsonify(Social.get_social(id_user,id_social))
+    return jsonify(social)
 
 @app.route('/users/<int:id_user>/socials/<int:id_social>/posts/<int:id_post>', methods=['GET'])
 def get_post(id_user,id_social,id_post):
-    if Post.get_post(id_user,id_social,id_post) is None:
+    post = Post.get_post(new_id_user,new_id_social,new_id_post)
+    if not post:
         raise APIException('Post not found', status_code=404) 
-    else:
-        return jsonify(Post.get_post(id_user,id_social,id_post))
+    return jsonify(post)
 
 @app.route('/users/<int:id_user>/socials/<int:id_social>/posts', methods=['GET'])
 def get_all_post(id_user,id_social):
-    if Post.get_all_post(id_user,id_social) is None:
+    posts = Post.get_all_post(id_user,id_social)
+    if not posts:
         raise APIException('Posts not found', status_code=404)
-    else: 
-        return jsonify(Post.get_all_post(id_user,id_social))
+    return jsonify(posts)
 
 @app.route('/users/<int:id_user>/socials/<int:id_social>/posts/<int:id_post>/multimedias', methods=['GET'])
 def get_all_multimedia(id_user,id_social,id_post):
-    if Multimedia.get_all_multimedia(id_user,id_social,id_post) is None:
+    multimedias = Multimedia.get_all_multimedia(id_user,id_social,id_post)
+    if not multimedias:
         raise APIException('Multimedia not found', status_code=404)
-    else: 
-        return jsonify(Multimedia.get_all_multimedia(id_user,id_social,id_post))
+    return jsonify(multimedias)
 
 @app.route('/users/<int:id_user>/socials/<int:id_social>/posts/<int:id_post>/multimedias/<int:id_multimedia>', methods=['GET'])
 def get_multimedia(id_user,id_social,id_post,id_multimedia):
-    if Multimedia.get_multimedia(id_user,id_social,id_post,id_multimedia) is None:
+    multimedia = Multimedia.get_multimedia(id_user,id_social,id_post,id_multimedia)
+    if not multimedia:
         raise APIException('Multimedia not found', status_code=404)
-    else: 
-        return jsonify(Multimedia.get_multimedia(id_user,id_social,id_post,id_multimedia))
+    return jsonify(multimedia)
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy_utils import PasswordType
 import enum
 
 db = SQLAlchemy()
@@ -10,9 +9,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
-    password = db.Column(PasswordType(schemes=['pbkdf2_sha512']),
-        unique=False,
-        nullable=False,)
+    password = db.Column(db.String(250), unique=False, nullable=False)
     email = db.Column(db.String(320), unique=True, nullable=False)
     name = db.Column(db.String(120), unique=False, nullable=False)
     is_active = db.Column(db.Boolean, default=True)
@@ -31,21 +28,16 @@ class User(db.Model):
     
     def get_user(new_id):
         user= User.query.filter_by(id=new_id,is_active=True).first()        
-        if user is None:
+        if not user:
             return None
         else:       
             return user.serialize()
     
     def get_user_by_email(new_email, new_password):
-        user = User.query.filter_by(email = new_email,is_active=True).first()
-        errors = ['email','password']
-        if user is None:
-            return errors[0]
-        elif new_password != user.password:
-            print(str(new_password) + " = " + str(user.password))
-            return errors[1]
-        else:
-            return user.serialize()
+        user = User.query.filter_by(email = new_email, password=new_password, is_active=True).first()
+        if not user:
+            return None
+        return user.serialize()
 
 
 
@@ -58,9 +50,7 @@ class SocialEnum(enum.Enum):
 class Social(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=False)
-    password = db.Column(PasswordType(schemes=['pbkdf2_sha512']),
-        unique=False,
-        nullable=False,)
+    password = db.Column(db.String(250), unique=False, nullable=False)
     email = db.Column(db.String(320), nullable=False, unique=False)
     social_name = db.Column(db.Enum("instagram","facebook","twitter","google"), nullable=False, unique=False)
     photo = db.Column(db.Text, nullable=True, unique=False)
@@ -84,7 +74,7 @@ class Social(db.Model):
     
     def get_all_socials(new_id):
         user = User.get_user(new_id)
-        if user is None:
+        if not user:
             return User.get_user(new_id)
         else:
             socials = Social.query.filter_by(user_id=new_id, is_active=True)
@@ -97,11 +87,11 @@ class Social(db.Model):
     
     def get_social(new_user_id, new_id_social):
         user = User.get_user(new_user_id)
-        if user is None:
+        if not user:
             return User.get_user(new_user_id)
         else:
             social = Social.query.filter_by(user_id=new_user_id,id=new_id_social,is_active=True).first()
-            if social is None:
+            if not social:
                 return None
             else:
                 return social.serialize()
@@ -127,11 +117,11 @@ class Post(db.Model):
     def get_post(new_id_user,new_id_social,new_id_post):
         social = Social.get_social(new_id_user,new_id_social)
         user = User.get_user(new_id_user)
-        if user is None or social is None:
+        if not user or not social:
             return None
         else:
             post = Post.query.filter_by(id_social=new_id_social,id=new_id_post,is_active=True).first()
-            if post is None:
+            if not post:
                 return None
             else:
                 return post.serialize()
@@ -139,7 +129,7 @@ class Post(db.Model):
     def get_all_post(new_id_user,new_id_social):
         social = Social.get_social(new_id_user,new_id_social)
         user = User.get_user(new_id_user)
-        if user is None or social is None:
+        if not user or not social:
             return None
         else:
             posts = Post.query.filter_by(id_social=new_id_social,is_active=True).first()
@@ -170,7 +160,7 @@ class Multimedia(db.Model):
         post = Post.get_post(new_id_user,new_id_social,new_id_post)
         social = Social.get_social(new_id_user,new_id_social)
         user = User.get_user(new_id_user)
-        if user is None or social is None or post is None:
+        if not user or not social or not post:
             return None
         else:
             multimedias = Multimedia.query.filter_by(id_post=new_id_post,is_active=True)
@@ -184,11 +174,11 @@ class Multimedia(db.Model):
         post = Post.get_post(new_id_user,new_id_social,new_id_post)
         social = Social.get_social(new_id_user,new_id_social)
         user = User.get_user(new_id_user)
-        if user is None or social is None or post is None:
+        if not user or not social or not post:
             return None
         else:
             multimedia = Multimedia.query.filter_by(id_post=new_id_post, id=new_id_multimedia,is_active=True).first()
-            if multimedia is None:
+            if not multimedia:
                 return None
             else:
                 return multimedia.serialize()
